2a4f2300382a7b770bb37e53ee25a86bb1e3ed2f
tnsapegina <143860753+tnsapegina@users.noreply.github.com>
2024-03-03 18:11:29 +0500

    Update Controller.java

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,1 +1,43 @@
+package org.example.controller;
 
+import org.example.model.Student;
+import org.example.model.StudentGroup;
+import org.example.model.Teacher;
+import org.example.model.User;
+import org.example.service.GroupService;
+import org.example.service.UserService;
+import org.example.view.StudentView;
+
+import java.util.List;
+
+public class Controller {
+    private UserService userService;
+    private GroupService groupService;
+    private StudentView studentView;
+
+    public Controller() {
+        this.userService = new UserService();
+        this.groupService = new GroupService();
+        this.studentView = new StudentView();
+    }
+
+    public Student createStudent(Student student) {
+        return (Student) userService.create(student);
+    }
+
+    public Teacher createTeacher(Teacher teacher) {
+        return (Teacher) userService.create(teacher);
+    }
+
+    public StudentGroup createGroup(Integer num, Teacher teacher, Student... students) {
+        return groupService.CreateGroup(num, teacher, students);
+    }
+
+    public void printStudents(Student student) {
+        studentView.printStudent(student);
+    }
+
+    public List<User> returnStudents() {
+        return userService.read();
+    }
+}
