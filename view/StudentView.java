cf6b1e47d793bc3c3c7a170e92668d5666ceac35
tnsapegina <143860753+tnsapegina@users.noreply.github.com>
2024-03-03 18:37:32 +0500

    Create StudentView.java

---
+++ view/StudentView.java
@@ -0,0 +1,11 @@
+import org.example.model.Student;
+
+public class StudentView {
+    public void printStudent(Student student){
+        System.out.println("Студент: " + student);
+    }
+    public void printAllStudent(){
+
+    }
+
+}
